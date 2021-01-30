7fbfa4dc8f089ab94f75527e8b38fa5a9521ad59
email <vivek_1rvd@rediffmail.com>
2021-01-31 00:03:58 +0530

    function

--- swirl.R
+++ swirl.R
@@ -1,9 +1,15 @@
 x <- -3
 if (x<0) {
-  
-  print("X is negetive ")
+    print("X is negetive ")
 } else if (x==0) {
   print("X is Zero")
 } else {
   print("x is positive")
+}
+
+x<-0
+repeat{
+  x<-x+5
+  print(x)
+  if (x==80) break
 }
